1d75aefddaca48e00d7c733acc1d6b03c79a629b
MohammedSIssa <mohammedsissa1999@gmail.com>
2025-06-09 14:05:09 +0300

    Library Update

--- library/routes/authorRouter.js
+++ library/routes/authorRouter.js
@@ -1,12 +1,11 @@
 const { Router } = require("express");
+const { getAuthorById } = require('../controllers/authorController');
+
 
 const authorRouter = Router();
 
 authorRouter.get("/", (req, res) => res.send("GET all authors"));
 
-authorRouter.get('/:authorId', (req, res) => {
-  const { authorId } = req.params;
-  res.send(`Author ID: ${authorId}`);
-})
+authorRouter.get('/:authorId', getAuthorById)
 
 module.exports = authorRouter;
